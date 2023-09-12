77e23d1cfb11e9cf932c9cff3e47e03ab01a382d
ted111712 <ted111712@gmail.com>
2023-09-12 16:28:11 +0900

    Add: Fist Upload
    - html, css 작업

--- myblogproject/myblogapp/views.py
+++ myblogproject/myblogapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
 # Create your views here.
+def write(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        Article.objects.create(title=title, content=content)
+        return redirect("post")
+    return render(request, "write.html")
+
+def post(request):
+    return render(request, "post.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def boardadmin(request):
+    return render(request, "board-admin.html")
+
+def boardclient(request):
+    return render(request, "board-client.html")
