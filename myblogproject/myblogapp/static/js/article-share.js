fd7ae7e59a49c6554680ed78e06c2536f778cb05
Seonggi <leeseonggi93@gmail.com>
2023-09-18 17:16:13 +0900

    ADD html, css, js
    -html
    1.article-share.html 추가
    2.post.html article-share연결, 수정삭제버튼 수정, 공유하기 수정
    3. write.html 수정
    4. board-client 삭제
    
    -css
    1. post.css 추가사항 - 버튼스타일
    
    -js
    article-share.js 추가

---
+++ myblogproject/myblogapp/static/js/article-share.js
@@ -0,0 +1,51 @@
+let postUrl = window.location.href;
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("share-link").value = postUrl;
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+function shareTwitter() {
+    // 트위터 공유 URL 설정
+    const shareUrl = `https://twitter.com/intent/tweet?url=${postUrl}&text=공유할 텍스트`;
+    window.open(shareUrl, "_blank");
+    closeModal(); // 팝업 닫기
+}
+
+function shareFacebook() {
+    // 페이스북 공유 URL 설정
+    const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${postUrl}`;
+    window.open(shareUrl, "_blank");
+    closeModal(); // 팝업 닫기
+}
+
+function copyToClipboard() {
+    // 클립보드에 URL 복사
+    let tempInput = document.createElement("input");
+    document.body.appendChild(tempInput);
+    tempInput.value = postUrl;
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+
+    // 알림 표시
+    alert("포스트 링크가 클립보드에 복사되었습니다");
+    closeModal();
+}
+
+function shareKakao() {
+    Kakao.init("8f7bd7a4d2f7d78d444873753b489269");
+
+    Kakao.Link.sendScrap({
+        requestUrl: postUrl,
+        templateId: 98566,
+        templateArgs: {
+            TITLE: "제목",
+            DESC: "설명",
+        },
+    });
+}
